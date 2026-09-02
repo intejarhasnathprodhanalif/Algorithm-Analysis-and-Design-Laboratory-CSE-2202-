84ab85d9ff999bfd3ffb2620215225deb8c1d47f
intejarhasnathprodhanalif <intejaralif@gmail.com>
2026-09-03 02:06:18 +0600

    Added shortestpath printing method

--- Dijkstra_Algorithm_using_vector.cpp
+++ Dijkstra_Algorithm_using_vector.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int INF=1e9;
+
 void addEdge(vector<vector<pair<int, int>>> &graph, int u, int v, int w)
 {
     graph[u].push_back({v, w});
@@ -28,7 +30,7 @@ void Dijkstra(vector<vector<pair<int, int>>> &graph, int src)
 {
     int V=graph.size();
 
-    vector<int> dist(V, INT_MAX);
+    vector<int> dist(V, INF);
     dist[src]=0;
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
@@ -65,11 +67,77 @@ void Dijkstra(vector<vector<pair<int, int>>> &graph, int src)
     cout << "Shortest distances from vertex " << src << ":" << endl;
     for (int i = 0; i < V; i++)
     {
-        if (dist[i] == INT_MAX)
+        if (dist[i] == INF)
             cout << i << ": unreachable" << endl;
         else
             cout << i << ": " << dist[i] << endl;
     }
+}
+
+void showPath(vector<vector<pair<int, int>>> &graph, int src, int dest)
+{
+    int V = graph.size();
+
+    vector<int> dist(V, INT_MAX);
+    vector<int> parent(V, -1); // Tracks the predecessor of each vertex
+    dist[src] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, src});
+
+    while (!pq.empty())
+    {
+        int d = pq.top().first;
+        int u = pq.top().second;
+
+        pq.pop();
+
+        if (d > dist[u])
+            continue;
+
+        // Optimization: Stop algorithm early if destination is fully processed
+        if (u == dest)
+            break;
+
+        for (auto edge : graph[u])
+        {
+            int v = edge.first;
+            int w = edge.second;
+
+            if (dist[v] > dist[u] + w)
+            {
+                dist[v] = dist[u] + w;
+                parent[v] = u; // Record that we reached 'v' from 'u'
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    // Path Reconstruction
+    if (dist[dest] == INT_MAX)
+    {
+        cout << "Vertex " << dest << " is unreachable from " << src << endl;
+        return;
+    }
+
+    // Trace backwards from destination using the parent array
+    vector<int> path;
+    for (int curr = dest; curr != -1; curr = parent[curr])
+    {
+        path.push_back(curr);
+    }
+
+    cout << "Shortest distance from " << src << " to " << dest << ": " << dist[dest] << endl;
+    cout << "Path: ";
+
+    // Print the path in reverse order (from start to end)
+    for (int i = path.size() - 1; i >= 0; i--)
+    {
+        cout << path[i];
+        if (i > 0)
+            cout << " -> ";
+    }
+    cout << endl;
 }
 
     int main()
@@ -107,6 +175,17 @@ void Dijkstra(vector<vector<pair<int, int>>> &graph, int src)
 
     Dijkstra(graph, src);
     cout << endl;
+
+    cout << "For Shortest path from a source to destination" << endl;
+    int dest;
+    cout << "Enter source: ";
+    cin >> src;
+    cout << endl;
+    cout << "Enter destination: ";
+    cin >> dest;
+    cout << endl;
+
+    showPath(graph, src, dest);
 
     return 0;
 }
